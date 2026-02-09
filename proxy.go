fcdc003a218c69d1f315808d6fd8eadb4f87785d
Clay Douglas <179662590+TuttersDaDemigod@users.noreply.github.com>
2026-02-09 17:35:13 -0600

    Init commit

---
+++ proxy.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+)
+
+type Backend struct {
+	URL            *url.URL
+	alive          bool
+	mu             sync.RWMutex
+	ReverseProxy   *httputil.ReverseProxy
+	HealthCheckURL string
+}
+
+func newBackend(backendURL string, healthCheckUrl string) (*Backend, error) {
+	url, err := url.Parse(backendURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Backend{
+		URL:            url,
+		alive:          true,
+		mu:             sync.RWMutex{},
+		ReverseProxy:   httputil.NewSingleHostReverseProxy(url),
+		HealthCheckURL: healthCheckUrl,
+	}, nil
+}
+
+func (backendServer *Backend) HealthCheck() (bool, string, error) {
+	// TODO: add support for other layers (currently only L7)
+	resp, err := http.Get(backendServer.URL.String() + backendServer.HealthCheckURL)
+	if err != nil {
+		return false, "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		// TODO: add support for other status codes and logging
+		return false, resp.Status, nil
+	}
+	return true, resp.Status, nil
+}
+
+func (backendServer *Backend) SetAlive(alive bool) {
+	backendServer.mu.Lock()
+	backendServer.alive = alive
+	defer backendServer.mu.Unlock()
+}
+
+func (backendServer *Backend) IsAlive() bool {
+	backendServer.mu.RLock()
+	defer backendServer.mu.RUnlock()
+
+	return backendServer.alive
+}
