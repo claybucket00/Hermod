671f09a989e6c23a67654830836489d7d767e0ec
claybucket00 <ckd6@illinois.edu>
2026-02-19 15:58:42 -0600

    push updated lb main

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -1,1 +1,20 @@
 package lb
+
+import (
+	"hermod/internal/config"
+	"hermod/internal/router"
+	"hermod/internal/serverpool"
+	"log"
+)
+
+func main() {
+	cfg, err := config.Load("config.yaml")
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	r := router.NewRouter()
+	for _, backend := range cfg.Backends {
+		pool := serverpool.NewServerPool(backend.Address)
+	}
+}
